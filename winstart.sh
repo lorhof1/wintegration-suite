f203973498ad4a0a22984266115f149c30632446
lorhof1 <61967687+lorhof1@users.noreply.github.com>
2021-04-14 16:59:48 +0200

    Update winstart.sh

--- winstart.sh
+++ winstart.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+#This script starts a parallel installed windows os.
+#Usage: bash winstart.sh
+#Example: bash winstart.sh
+
+#todo: search for how to change boot entries on stackoverflow, add find win efi, add restart
