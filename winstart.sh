ffe85d2a52c309fd02737c7ecdca0256bcb49fdc
lorhof1 <61967687+lorhof1@users.noreply.github.com>
2021-04-17 14:34:58 +0200

    Update winstart.sh

--- winstart.sh
+++ winstart.sh
@@ -3,4 +3,9 @@
 #Usage: bash winstart.sh
 #Example: bash winstart.sh
 
-#todo: search for how to change boot entries on stackoverflow, add find win efi, add restart
+echo "winstart script by lorhof1 (part of wintegration suite) | github.com/lorhof1/wintegration-suite"
+echo "Please enter your Password"
+sudo su
+grub-set-default $win
+update-grub
+reboot
